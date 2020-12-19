87c6704ad0d49a2d8c8763d20010040fd322035e
SoftwareTestingByRajTripathi <tripathideva@gmail.com>
2020-12-19 19:19:55 +0530

    Static program

---
+++ Automation_Testing/src/test/java/program/StaticVariable1.java
@@ -0,0 +1,14 @@
+package program;
+
+public class StaticVariable1 {
+	
+	static int a=10;
+	static String str ="Shristi";
+	public static void main(String[] args) {
+
+	System.out.println("Integer value"+a);
+	System.out.println("Strin Value"+str);
+		
+	}
+}
+
